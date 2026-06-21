d9935a1b3e417a8d27c2130972f585bf7bab307e
fahim-md <msmaimrfahim@gmail.com>
2026-06-21 11:25:01 +0600

    Update image URL in sendMessage function

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ console.log('Bot connected to whatsapp ✅')
 
   let up = `Hasi-Ai connected successful ✅\n\nPREFIX: ${prefix}`;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/900435c6d3157c98c3c88.jpg` }, caption: up })
+conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://i.ibb.co/F4y648g3/9bb0e4c75bae.jpg` }, caption: up })
 
 }
 })
